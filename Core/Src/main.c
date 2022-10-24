43dcdaab62dbc901d7428b3e304532a7c4943af9
AGarcesS <60661868+AGarcesS@users.noreply.github.com>
2022-10-24 12:30:08 +0200

    Modificado: evaluación del timer de botón movida a la función de callback

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -39,7 +39,7 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
-#define tiempo_boton 500
+#define TIEMPO_BOTON 500
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -85,7 +85,6 @@ static uint8_t sensorx = 0, sensory = 0, sensorz = 0;
 
 //variables compartidas
 static uint8_t activado = 0, timer_boton = 1, timer_lectura = 0, timer_led = 0;
-uint32_t contador_tiempo_boton=0;
 
 //salidas
 static uint8_t faultx, faulty, faultz;
@@ -292,13 +291,6 @@ int main(void)
     fsm_fire (fsm_lectura_z);
     fsm_fire (fsm_led_activo);
 
-    if (contador_tiempo_boton> tiempo_boton){
-    	timer_boton=1;
-    }
-    else
-    {
-    	timer_boton=0;
-    }
     if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1))
     	sensorx = 1;
     else
@@ -365,8 +357,11 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
 	//Comprobar que viene del timer 2
 	if (htim->Instance==TIM2){
 		boton=~boton;
-		contador_tiempo_boton = __HAL_TIM_GetCounter(htim);
-			__HAL_TIM_SetCounter(htim,0);
+		if(__HAL_TIM_GetCounter(htim) > TIEMPO_BOTON)
+			timer_boton = 1;
+		else
+			timer_boton = 0;
+		__HAL_TIM_SetCounter(htim,0);
 	}
 }
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
